3d6baecc907b4b214fd456d1ce55abd649f4e609
Henhrie <akinolahenryayobami@gmail.com>
2023-11-05 00:15:02 +0800

    Data Structures

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    Print each integer in the given list.
+
+    Args:
+        my_list (list): A list of integers.
+
+    Returns:
+        None
+    """
+    for i in my_list:
+        print("{:d}".format(i))
