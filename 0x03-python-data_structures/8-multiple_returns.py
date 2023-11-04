86fce8b5da5b3a1bcf67a00c459dd800c3d317a1
Henhrie <akinolahenryayobami@gmail.com>
2023-11-05 02:37:57 +0800

    Data Structurs

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
-"""
-Calculate the length of the sentence and return a tuple containing the length of the
-    sentence and the first character of the sentence.
-
-Parameters:
-    sentence (str): The input sentence.
-
-Returns:
-
-tuple: A tuple containing the length of the sentence and the first character
-    of the sentence.
-"""
     my_tuple = ()
     if len(sentence) == 0:
         my_tuple = 0, "None"
